9e220afd0eb370969a96b6ace23f774e45d0485b
Michael Buholzer <michael.buholzer@outlook.com>
2017-06-25 12:46:16 +0200

    added custom karma launcher for travis ci

--- karma.conf.js
+++ karma.conf.js
@@ -22,12 +22,20 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: process.env.TRAVIS 
+      ? ['Chrome_travis_ci'] 
+      : ['Chrome'],
     singleRun: false
   });
 };
