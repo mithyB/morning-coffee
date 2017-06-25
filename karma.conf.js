b8d9feb54305d27ccc4ff84b9c684c8ac91af555
Michael Buholzer <michael.buholzer@outlook.com>
2017-06-25 12:55:42 +0200

    use chrome for unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: process.env.TRAVIS 
-      ? ['Chrome_travis_ci'] 
+      ? ['Chrome'] 
       : ['Chrome'],
     singleRun: false
   });
